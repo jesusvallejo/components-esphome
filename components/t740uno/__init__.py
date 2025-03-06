02dfdbd4368d58a540cccc344fa4cdee03a146b4
Jesús Vallejo <jesus.vallejo.collados@gmail.com>
2025-03-06 11:42:22 +0100

    Update __init__.py

--- components/t740uno/__init__.py
+++ components/t740uno/__init__.py
@@ -1,7 +1,8 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import automation, pins
-from esphome.components import uart 
+from esphome.components import uart
+from esphome.codegen import Action  # Explicitly import Action here!
 from esphome.components import binary_sensor
 from esphome.automation import maybe_simple_id
 from esphome.const import (
