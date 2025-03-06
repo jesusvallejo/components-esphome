20605ac2b9ae6a766c6e6b5916e41be0b3758a4a
Jesus Vallejo Collados <jesus.vallejo.collados@gmail.com>
2025-03-06 16:49:20 +0100

    11

--- components/t740uno/t740uno.h
+++ components/t740uno/t740uno.h
@@ -10,7 +10,7 @@
 namespace esphome {
 namespace t740uno {
 
-class T740UNO : public Component, public uart::UARTDevice {
+class T740UNOComponent : public Component, public uart::UARTDevice {
 #ifdef USE_BINARY_SENSOR
    SUB_BINARY_SENSOR(calling_alert)
 #endif
