2529e5f38f28d5269721192aea7ff29671325d5c
Jesus Vallejo Collados <jesus.vallejo.collados@gmail.com>
2025-03-06 17:15:30 +0100

    sdv

--- components/t740uno/__init__.py
+++ components/t740uno/__init__.py
@@ -11,18 +11,20 @@ CONF_T740UNO_ID = "t740uno_id"
 
 
 t740uno_ns = cg.esphome_ns.namespace('t740uno')
-T740UNO = t740uno_ns.class_('T740UNOComponent', cg.Component, uart.UARTDevice)
+T740UNOComponent = t740uno_ns.class_('T740UNOComponent', cg.Component, uart.UARTDevice)
 
 CONF_UART_ID = 'uart_id'
 CONF_INCOMING_CALL = 'incoming_call'
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(T740UNO),
-    cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent)
+CONFIG_SCHEMA = (cv.Schema({
+    cv.GenerateID(): cv.declare_id(T740UNOComponent),
 })
+    .extend(uart.UART_DEVICE_SCHEMA)
+    .extend(cv.COMPONENT_SCHEMA)
+)
 
 async def to_code(config):
-    uart_component = await cg.get_variable(config[CONF_UART_ID])
-    var = cg.new_Pvariable(config[CONF_ID], uart_component)
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
 
