890bbe8dc8b8bbcabda528c6405efad1521afd54
Jesus Vallejo Collados <jesus.vallejo.collados@gmail.com>
2025-03-06 18:32:41 +0100

    open door

--- components/t740uno/button/open_door_button.cpp
+++ components/t740uno/button/open_door_button.cpp
@@ -3,7 +3,7 @@
 namespace esphome {
 namespace t740uno {
 
-void ResetRadarButton::press_action() { this->parent_->open(); }
+void OpenDoorButton::press_action() { this->parent_->open(); }
 
 }  // namespace t740uno
 }  // namespace esphome
