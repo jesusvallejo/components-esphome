8f8472a33ce18bdb08f122686180ea64c009ffb7
Jesus Vallejo Collados <jesus.vallejo.collados@gmail.com>
2025-03-06 12:53:11 +0100

    1

--- components/t740uno/t740uno.cpp
+++ components/t740uno/t740uno.cpp
@@ -15,38 +15,8 @@ void T740UNO::loop() {
 }
 
 
-void MegaDesk::recvData_() {
-    const int numChars = 2;
-    const int numFields = 4; // read/store all 4 fields for simplicity, use only the last 3.
-    // static variables allows segmented/char-at-a-time decodes
-    static uint16_t receivedBytes[numFields];
-    static uint8_t ndx = 0;
-    int r; // read char/digit
-  
-    // read 2 chars
-    while ((ndx < numChars) && ((r = read()) != -1))
-    {
-      if ((ndx == 0) && (r != '>'))
-      {
-        // first char is not Tx, keep reading...
-        continue;
-      }
-      receivedBytes[ndx] = r;
-      ++ndx;
-    }
-    // read ascii digits
-    while ((ndx >= numChars) && ((r = readdigits_()) != -1)) {
-      receivedBytes[ndx] = r;
-      this->digits_ = 0; // clear
-      if (++ndx == numFields) {
-        // thats all 4 fields. parse/process them now and break-out.
-        parseData_(receivedBytes[1],
-                   receivedBytes[2],
-                   receivedBytes[3]);
-        ndx = 0;
-        return;
-      }
-    }
+void T740UNO::recvData_() {
+    this->ring_->publish_state(1);
   }
   
 
