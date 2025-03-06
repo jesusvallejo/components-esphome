72ed93ca4bd07d60bea573c7c5eea8e92650e195
Jesus Vallejo Collados <jesus.vallejo.collados@gmail.com>
2025-03-06 16:36:16 +0100

    2

--- components/t740uno/__init__.py
+++ components/t740uno/__init__.py
@@ -11,7 +11,7 @@ CONF_T740UNO_ID = "t740uno_id"
 
 
 t740uno_ns = cg.esphome_ns.namespace('t740uno')
-T740UNO = t740uno_ns.class_('T740UNOcomponent', cg.Component, uart.UARTDevice)
+T740UNO = t740uno_ns.class_('T740UNOComponent', cg.Component, uart.UARTDevice)
 
 CONF_UART_ID = 'uart_id'
 CONF_INCOMING_CALL = 'incoming_call'
