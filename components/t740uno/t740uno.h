ea5e3c55be32fff4236ea3f89dd7e992fc762d24
Jesus Vallejo Collados <jesus.vallejo.collados@gmail.com>
2025-03-06 13:22:05 +0100

    4

--- components/t740uno/t740uno.h
+++ components/t740uno/t740uno.h
@@ -2,7 +2,7 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/uart/uart.h"
-#include "esphome/components/sensor/sensor.h"
+// #include "esphome/components/sensor/sensor.h"
 
 namespace esphome {
 namespace t740uno {
