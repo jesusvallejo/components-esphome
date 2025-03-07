43bc43ace98e3de5498419d29a416ded1e32aa83
Jesus Vallejo Collados <jesus.vallejo.collados@gmail.com>
2025-03-07 09:39:21 +0100

    123

--- components/t740uno/button/open_door_button.h
+++ components/t740uno/button/open_door_button.h
@@ -9,7 +9,8 @@ namespace t740uno {
 class OpenDoorButton : public button::Button, public Parented<T740UNOComponent> {
  public:
   OpenDoorButton() = default;
-  //void dump_config() override;
+  
+  void dump_config() override;
 
  protected:
   void press_action() override;
