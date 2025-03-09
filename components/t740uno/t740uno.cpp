6d242ea053a74fc115ef8e9b1e7ccc0eaafd8fc0
Jesús Vallejo <jesus.vallejo.collados@gmail.com>
2025-03-09 19:58:24 +0100

    Update t740uno.cpp

--- components/t740uno/t740uno.cpp
+++ components/t740uno/t740uno.cpp
@@ -30,6 +30,33 @@ void T740UNOComponent::loop() {
   }
 }
 
+
+void T740UNOComponent::loop() {
+    static const uint8_t target_payload[] = {0x80, 0x80, 0xc9, 0xdb, 0x80, 0x80, 0xc9, 0xc1};
+    static size_t match_index = 0;
+
+    while (available()) {
+      uint8_t byte = read();
+      if (byte == target_payload[match_index]) {
+        match_index++;
+        if (match_index == sizeof(target_payload)) {
+                ESP_LOGD(TAG, "Incoming call detected");
+        #ifdef USE_BINARY_SENSOR
+            if (this->calling_alert_binary_sensor_ != nullptr) {
+                this->calling_alert_binary_sensor_->publish_state(true);
+                this->set_timeout(1000, [this]() { this->calling_alert_binary_sensor_->publish_state(false); });
+            }
+        #endif
+          match_index = 0;
+          
+        }
+      } else {
+        match_index = (byte == target_payload[0]) ? 1 : 0;
+      }
+    }
+}
+
+
 void T740UNOComponent::open() {
   ESP_LOGD(TAG, "Open command sent");
   this->write_byte(0x34);
