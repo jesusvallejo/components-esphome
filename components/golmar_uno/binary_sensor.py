528b112caf3de4e59f6c07accb5993ddfc94f910
Jesus Vallejo Collados <jesus.vallejo.collados@gmail.com>
2025-11-20 17:52:06 +0100

    ewrwer

--- components/golmar_uno/binary_sensor.py
+++ components/golmar_uno/binary_sensor.py
@@ -2,21 +2,22 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
 # from esphome.const import DEVICE_CLASS_DOORBELL
-from . import GolmarUnoComponent, CONF_GOLMAR_UNO_ID
+from . import GolmarUnoComponent, CONF_GOLMAR_UNO_ID,golmar_uno_ns
+
 
 DEPENDENCIES = ["golmar_uno"]
 
-CONF_CALLING_ALERT = "calling_alert"
+incoming_call = golmar_uno_ns.class_("incoming_call", binary_sensor.BinarySensor)
 
-CONFIG_SCHEMA = cv.Schema(
+CONFIG_SCHEMA = binary_sensor.binary_sensor(incoming_call).extend(
     {
         cv.GenerateID(CONF_GOLMAR_UNO_ID): cv.use_id(GolmarUnoComponent),
-        cv.Required(CONF_CALLING_ALERT): binary_sensor.binary_sensor_schema(),
     }
 )
 
 
 async def to_code(config):
-    hub = await cg.get_variable(config[CONF_CALLING_ALERT])
-    var = await binary_sensor.new_binary_sensor(config[CONF_CALLING_ALERT])
-    cg.add(hub.set_calling_alert_binary_sensor(var))
+    hub = await cg.get_variable(config[CONF_GOLMAR_UNO_ID])
+    b = await binary_sensor.new_binary_sensor(config)
+    await cg.register_parented(b, hub)
+    cg.add(hub.set_incoming_call(b))
