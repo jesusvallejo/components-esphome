3cc4449f3480d4a03fa2342e6a4755e4b2c24603
Jesus Vallejo Collados <jesus.vallejo.collados@gmail.com>
2025-11-20 16:06:04 +0100

    ii

--- components/golmar_uno/binary_sensor.py
+++ components/golmar_uno/binary_sensor.py
@@ -2,9 +2,11 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
 # from esphome.const import DEVICE_CLASS_DOORBELL
-from . import GOLMAR_UNO, CONF_GOLMAR_UNO_ID
+from . import GOLMAR_UNO, CONF_GOLMAR_UNO_ID,golmar_uno_ns
 
 DEPENDENCIES = ["golmar_uno"]
+
+GOLMAR_UNO = golmar_uno_ns.class_("GolmarUnoBinarySensor", binary_sensor.BinarySensor)
 
 CONFIG_SCHEMA = binary_sensor.binary_sensor_schema(
     # device_class=DEVICE_CLASS_DOORBELL
@@ -16,7 +18,7 @@ CONFIG_SCHEMA = binary_sensor.binary_sensor_schema(
 
 
 async def to_code(config):
-    hub = await cg.get_variable(config[CONF_GOLMAR_UNO_ID])
     var = await binary_sensor.new_binary_sensor(config)
+    hub = await cg.get_variable(config[CONF_GOLMAR_UNO_ID])
     await cg.register_parented(var, hub)
     cg.add(hub.set_calling_alert_binary_sensor(var))
