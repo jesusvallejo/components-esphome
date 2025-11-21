772c8151f6a6ba019309a055a3161735154d79ff
Jesus Vallejo Collados <jesus.vallejo.collados@gmail.com>
2025-11-21 08:47:09 +0100

    ji

--- components/golmar_uno/golmar_uno.cpp
+++ components/golmar_uno/golmar_uno.cpp
@@ -8,8 +8,8 @@ namespace golmar_uno {
 static const char *TAG = "golmar_uno.component";
 
 void golmar_uno_component::dump_config() {
-  LOG_BINARY_SENSOR(" ", "Incoming Call Binary Sensor", this->calling_alert_binary_sensor_);
-  LOG_BUTTON(" ", "Open Door Button", this->open_door_button_);
+  //LOG_BINARY_SENSOR(" ", "Incoming Call Binary Sensor", this->calling_alert_binary_sensor_);
+ // LOG_BUTTON(" ", "Open Door Button", this->open_door_button_);
 }
 
 
