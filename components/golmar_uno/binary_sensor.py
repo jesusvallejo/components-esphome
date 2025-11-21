10894e27336515fa45a76d17eeb04fc872f26275
Jesus Vallejo Collados <jesus.vallejo.collados@gmail.com>
2025-11-21 09:42:11 +0100

    www

--- components/golmar_uno/binary_sensor.py
+++ components/golmar_uno/binary_sensor.py
@@ -2,12 +2,14 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
 
-from . import GolmarUnoComponent, CONF_GOLMAR_UNO_ID
+from . import GolmarUnoComponent, CONF_GOLMAR_UNO_ID, golmar_uno_ns
 
 
 DEPENDENCIES = ["golmar_uno"]
 
-CONFIG_SCHEMA = binary_sensor.binary_sensor_schema(binary_sensor.BinarySensor).extend(
+calling_alert_ns = golmar_uno_ns.class_("calling_alert", binary_sensor.BinarySensor)
+
+CONFIG_SCHEMA = binary_sensor.binary_sensor_schema(calling_alert_ns).extend(
     {
         cv.GenerateID(CONF_GOLMAR_UNO_ID): cv.use_id(GolmarUnoComponent),
     }
