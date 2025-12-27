9d19c3c888785d6d60d2a7a6a65a83a6d417ce1d
Jesús Vallejo <jesus.vallejo.collados@gmail.com>
2025-12-27 12:37:57 +0100

    fix

--- components/golmar_uno/lock/__init__.py
+++ components/golmar_uno/lock/__init__.py
@@ -17,9 +17,9 @@ CONFIG_SCHEMA = lock.lock_schema(door_lock_ns).extend(
 
 async def to_code(config):
     hub = await cg.get_variable(config[CONF_GOLMAR_UNO_ID])
-    b = await lock.new_door_lock(config)
+    b = await lock.new_lock(config)
     await cg.register_parented(b, hub)
-    cg.add(hub.set_door_lock_(b))
+    cg.add(hub.set_lock_(b))
 
 
 
