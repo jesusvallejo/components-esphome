8f55156c4fd16adad00558b68e33b2cbfd4a1c5a
Jesús Vallejo <jesus.vallejo.collados@gmail.com>
2025-12-27 12:30:33 +0100

    add lock support

---
+++ components/golmar_uno/lock/__init__.py
@@ -0,0 +1,28 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import button
+
+from .. import GolmarUnoComponent, CONF_GOLMAR_UNO_ID,golmar_uno_ns
+
+DEPENDENCIES = ["golmar_uno"]
+
+open_door_button_ns = golmar_uno_ns.class_("open_door_button", button.Button)
+
+CONFIG_SCHEMA = button.button_schema(open_door_button_ns).extend(
+    {
+        cv.GenerateID(CONF_GOLMAR_UNO_ID): cv.use_id(GolmarUnoComponent),
+    }
+)
+
+
+async def to_code(config):
+    hub = await cg.get_variable(config[CONF_GOLMAR_UNO_ID])
+    b = await button.new_button(config)
+    await cg.register_parented(b, hub)
+    cg.add(hub.set_open_door_button_(b))
+
+
+
+
+
+    
