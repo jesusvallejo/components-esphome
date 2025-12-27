bd3c40e060ceca0cccad36890bea3f840ae0c0e3
Jesús Vallejo <jesus.vallejo.collados@gmail.com>
2025-12-27 20:03:15 +0100

    more delay

--- components/golmar_uno/lock/door_lock.cpp
+++ components/golmar_uno/lock/door_lock.cpp
@@ -17,7 +17,7 @@ void door_lock::lock() {
     // For this simple implementation, we do not support locking via the lock entity.
     ESP_LOGW(TAG, "Lock requested via lock entity, but locking is not supported. reseting to locked state.");
     if (this->parent_ != nullptr) {
-            this->parent_->schedule_door_lock(4000);
+            this->parent_->schedule_door_lock(10000);
         }
 }
 
